94c9041abbfa7489ae7e2f933b670212b39cd727
Mathias Standaert <mathias.standaert@student.howest.be>
2018-09-28 15:37:00 +0200

    notifications

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,13 @@
-
-    // Initialize Firebase
-    var config = {
-        apiKey: "AIzaSyDMFDDjbk7tOEeqsO_evebul166VQl4CR0",
-        authDomain: "first-lesson-a81e0.firebaseapp.com",
-        databaseURL: "https://first-lesson-a81e0.firebaseio.com",
-        projectId: "first-lesson-a81e0",
-        storageBucket: "first-lesson-a81e0.appspot.com",
-        messagingSenderId: "213223109314"
-    };
-    firebase.initializeApp(config);
+// Initialize Firebase
+var config = {
+    apiKey: "AIzaSyDMFDDjbk7tOEeqsO_evebul166VQl4CR0",
+    authDomain: "first-lesson-a81e0.firebaseapp.com",
+    databaseURL: "https://first-lesson-a81e0.firebaseio.com",
+    projectId: "first-lesson-a81e0",
+    storageBucket: "first-lesson-a81e0.appspot.com",
+    messagingSenderId: "213223109314"
+};
+firebase.initializeApp(config);
 
 var database = firebase.database();
 var ref = database.ref("messages/")
@@ -17,107 +16,107 @@ var nameSet = false
 var focused = true
 var original = "MattChatt"
 
-function changeName (name){
+function changeName(name) {
     yourname = name
 }
 
-function notify (bericht){
+function notify(bericht) {
     window.document.title = "berichtje!!!"
 
 
-    if (!Notification.permission==="granted"){
+    if (!Notification.permission === "granted") {
         Notification.requestPermission()
     }
-   
+
     var not = new Notification("berichtje", {
         body: "Je hebt een bericht gekregen op MattChatt"
     });
 
 
-    
 }
 
-function cleanChat(data){
+function cleanChat(data) {
     var removed = 0
-    while((Object.keys(data)).length-removed >250){
-    ref.child(Object.keys(data)[0]).remove()
-    removed += 1 
-    
+    while ((Object.keys(data)).length - removed > 250) {
+        ref.child(Object.keys(data)[0]).remove()
+        removed += 1
 
-    
+
     }
 
 
 }
 
 function getMessage(data) {
+    notifyMe();
     var messages = Object.values(data.val());
 
     document.getElementById("chatWindow").innerHTML = ""
-   messages.forEach((bericht)=>{
+    messages.forEach((bericht) => {
 
-        message = document.createElement("div");
-        if(Object.keys(bericht)[0] === localStorage.getItem("username")){
-            message.setAttribute("class", "messageBox me");
-        } else {
-            message.setAttribute("class", "messageBox");
+            message = document.createElement("div");
+            if (Object.keys(bericht)[0] === localStorage.getItem("username")) {
+                message.setAttribute("class", "messageBox me");
+            } else {
+                message.setAttribute("class", "messageBox");
+            }
+            var name = document.createElement("span");
+            var textAndTime = document.createElement("div");
+            var text = document.createElement("span");
+            var time = document.createElement("span");
+            textAndTime.classList.add("textTime");
+            name.innerText = Object.keys(bericht)[0];
+            text.innerText = Object.values(bericht)[0];
+            time.innerText = Object.values(bericht)[1];
+            name.setAttribute("class", "name");
+            text.setAttribute("class", "message");
+            time.setAttribute("class", "time");
+            message.appendChild(name);
+            textAndTime.appendChild(text);
+
+            textAndTime.appendChild(time);
+            message.appendChild(textAndTime);
+            document.getElementById("chatWindow").appendChild(message)
+
+
         }
-        var name = document.createElement("span");
-        var textAndTime = document.createElement("div");
-        var text = document.createElement("span");
-        var time = document.createElement("span");
-        textAndTime.classList.add("textTime");
-        name.innerText = Object.keys(bericht)[0];
-        text.innerText = Object.values(bericht)[0];
-        time.innerText = Object.values(bericht)[1];
-        name.setAttribute("class", "name");
-        text.setAttribute("class", "message");
-        time.setAttribute("class", "time");
-        message.appendChild(name);
-        textAndTime.appendChild(text);
-        
-        textAndTime.appendChild(time);
-        message.appendChild(textAndTime);
-        document.getElementById("chatWindow").appendChild(message)
-        
-        
+    )
+
+    updateScroll();
+
+    if (!focused) {
+        notify(messages[messages.length - 1])
     }
-    
-   
-)
 
-updateScroll();
-
-if(!focused){notify(messages[messages.length-1])}
-
-cleanChat(data.val())
+    cleanChat(data.val())
 
 }
-
 
 
 function sendMessage(message) {
     var d = new Date
-   var timeString = d.getHours()
-   var min = d.getMinutes()
-    if (d.getMinutes()<10){
-         min = "0"+min  
+    var timeString = d.getHours()
+    var min = d.getMinutes()
+    if (d.getMinutes() < 10) {
+        min = "0" + min
     }
     timeString = timeString + ":" + min
-    database.ref("messages/").push(JSON.parse('{"'+yourname+'":"'+message+'", "timestamp":"'+  timeString +'"}'));
+    database.ref("messages/").push(JSON.parse('{"' + yourname + '":"' + message + '", "timestamp":"' + timeString + '"}'));
 }
-function sendFromPage(e){
+
+function sendFromPage(e) {
     e.preventDefault();
-    if(nameSet){
-    message = document.getElementById("mess").value;
-    document.getElementById("mess").value = "";
-    sendMessage(message)}
-    else{
+    if (nameSet) {
+        message = document.getElementById("mess").value;
+        document.getElementById("mess").value = "";
+        sendMessage(message)
+    }
+    else {
         alert("please choose a name")
     }
 }
 
-function changeNameFromPage(e){
+function changeNameFromPage(e) {
     e.preventDefault();
     const name = document.getElementById("changeName").value;
     localStorage.setItem("username", name);
@@ -126,12 +125,13 @@ function changeNameFromPage(e){
     nameSet = true;
 
 }
+
 $(document).ready(function () {
     document.getElementById("send").addEventListener("click", sendFromPage);
     document.getElementById("changeNameButton").addEventListener("click", changeNameFromPage);
 
     const name = localStorage.getItem("username");
-    if(name !== null){
+    if (name !== null) {
         changeName(name);
         nameSet = true;
         $("#nameForm").remove();
@@ -142,20 +142,46 @@ $(document).ready(function () {
 });
 
 
-function updateScroll(){
+function updateScroll() {
     var element = document.getElementById("chatWindow");
     element.scrollTop = element.scrollHeight;
 
 }
 
-window.onfocus = function(){
+window.onfocus = function () {
 
-focused = true
-window.document.title = original
+    focused = true
+    window.document.title = original
 
 }
-window.onblur = function(){
+window.onblur = function () {
     original = window.document.title
     focused = false
+
+}
+document.addEventListener('DOMContentLoaded', function () {
+    if (!Notification) {
+        alert('Desktop notifications not available in your browser. Try Chromium.');
+        return;
+    }
+
+    if (Notification.permission !== "granted")
+        Notification.requestPermission();
+});
+
+function notifyMe() {
+    if (Notification.permission !== "granted")
+        Notification.requestPermission();
+    else {
+        var notification = new Notification('MattChatt', {
+            icon: 'https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwia-O2x6N3dAhXRGuwKHR7SDNEQjRx6BAgBEAU&url=https%3A%2F%2Fwww.iconfinder.com%2Ficons%2F298330%2Fchat_bubble_conversation_messages_talk_icon&psig=AOvVaw2uvpmCzflCstwuZRhIJgN9&ust=1538228107827082',
+            body: "You got a new Message!!!!",
+        });
+
+        notification.onclick = function () {
+            window.open("https://bruynooghematthias.github.io/MattChatt/");
+        };
+
+    }
 
 }
